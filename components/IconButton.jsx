262790fbe731d7cabecaa899d2eec2a0d9d3cb0e
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2023-07-18 07:21:16 +0200

    add new assets

--- components/IconButton.jsx
+++ components/IconButton.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Pressable, View } from 'react-native'
+import { StyleSheet, Pressable } from 'react-native'
 import { Ionicons } from '@expo/vector-icons'
 const IconButton = ({ onPress, color, name }) => {
   return (
